6a6577ed7341e76e4a8b550c5f94044060214aec
SCHO <112309011+segene99@users.noreply.github.com>
2023-01-26 18:31:45 +0900

    Add files via upload
    test 2023.01.26 - sub query, join

--- test.sql
+++ test.sql
@@ -284,3 +284,276 @@ create table emp08 (
 );
 
 insert into emp08 values(15, '영업', '오리',234,'새새');
+
+----------------------------2023.01.26 END---------------------------------------
+
+select dname from dept
+where deptno = (select deptno from emp where ename = 'JONES');
+
+--smith와 같은 부서에서 근무하는 사원의 정보를 출력
+--출력내용 : 사번, 이름, 직책, 입사일자, 상위관리자만 출력
+select deptno, ename, job, hiredate, mgr from emp
+where deptno = (select deptno from emp where ename = 'SMITH') and not ename='SMITH';
+
+--서브 쿼리에서 그룹함수의 사용
+--평균 급여보다 더 많은 급여를 받는 사원을 검색
+--더많은 순으로 정렬
+select ename, sal from emp
+where sal > (select avg(sal) from emp) order by sal desc;
+
+select * from emp order by sal;
+
+select empno, ename from emp
+where sal = (select max(sal) from emp where deptno = 10);
+
+select deptno, dname from dept
+where not deptno = (select deptno from emp where ename = 'KING');
+
+--king의 부서가 아닌 사람들의 부서번호와 부서명을 출력
+
+
+/* 
+서브쿼리: 
+where deptno in(select deptno != (select deptno from emp where ename ='KING'));
+메인쿼리:
+select deptno, dname from dept
+*/
+--답
+select deptno, dname from dept
+where deptno in(
+select distinct deptno from emp
+where deptno != (select deptno from emp where ename = 'KING')
+);
+
+--not in 연산자
+--blake와 다른 부서에서 근무하는 사람들의 이름과 고용일을 출력
+
+select ename, hiredate from emp
+where deptno not in (select deptno from emp where ename ='BLAKE');
+
+--급여를 3000이상 받는 사원과 동일한 부서에서 일하는 사원들의 사원명과 급여, 부서번호를 출력
+
+--서브쿼리
+-- where deptno in (select deptno from emp where sal >= 3000)
+--메인쿼리
+--select ename, sal, deptno from emp
+--답
+--select ename, sal, deptno from emp where deptno in (select deptno from emp where sal >= 3000);
+
+--select ename, sal, deptno from emp
+
+--부서별로 나누어 최소 급여인 사람들과 동일한 급여를 가진 사람들의 이름, 급여 부서번호를 출력
+select ename, sal, deptno from emp 
+where sal in (select min(sal) from emp group by deptno) order by deptno, sal;
+
+select ename, sal, deptno from emp
+where sal in (select sal from emp where sal between 2000 and 10000) order by deptno;
+
+SELECT * from emp where sal < all(1000,2000,3000) order by sal; 
+select * from emp where sal > all(1000,2000,3000) order by sal;
+
+--부서번호가 30번인 사원들의 급여 중 가장 큰 값(2850) 보다 많은 급여를 받는 사람의 이름, 급여를 출력
+select ename, sal from emp where sal > all(select sal from emp where deptno = 30) order by sal;
+--부서번호가 30번인 사원들의 급여 중 가장 작은 값 보다 적은 급여를 받는 사람의 이름, 급여를 출력
+select ename, sal from emp where sal < all(select sal from emp where deptno = 30) order by sal;
+
+--부서별의 최고 평균급여를 넘는 사원출력
+select * from emp where sal > all(select avg(sal) from emp group by deptno) and deptno = 10 order by sal;
+
+SELECT * from emp where sal < any(1000,2000,3000) order by sal;
+select * from emp where sal > any(1000,2000,3000) order by sal;
+
+--부서번호가 30번인 사원들의 급여 중 --서브
+--최소급여보다 많은 급여를 받는 --서브
+--사원의 이름, 급여를 출력 --메인쿼리
+
+select ename, sal from emp
+where sal > any(select sal from emp where deptno = 30) order by sal;
+
+--부서번호가 30번인 사원들의 급여 중 가장 큰 값보다 적은 급여를 받는 사원의 이름, 급여를 출력
+select ename, sal from emp where sal < any(select sal from emp where deptno=30);
+
+
+--점원이 받는 최고급여보다 더 적게 받으면서 점원이 아닌 사원의 사번, 이름, 직책, 급여를 출력하세요
+select empno, ename, job, sal from emp
+where sal < any(select sal from emp where job = 'CLERK') and job <> 'CLERK';
+
+create table emp04 as select * from emp where 1=0;
+drop table emp04;
+drop table emp05;
+
+insert all into emp04 values (empno, ename, job, mgr, hiredate, sal, comm, deptno) select * from emp;
+
+drop table emp06;
+drop table emp07;
+drop table emp08;
+
+desc emp04;
+select * from emp04;
+
+rollback;
+
+insert into emp04 select * from emp;
+
+rollback;
+
+insert all
+into emp04
+(empno, ename, deptno)
+values (empno, ename, deptno)
+select empno, ename, deptno from emp;
+
+insert
+into emp04
+(empno, ename, deptno)
+select empno, ename, deptno from emp;
+
+insert all
+into emp04
+into emp05
+values (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+select * from emp;
+
+alter table emp04 drop (mgr, hiredate, sal, comm, deptno);
+
+alter table emp05 drop (hiredate, sal, comm, deptno);
+
+insert all
+into emp04 values (empno, ename, job)
+into emp05 values (empno, ename, job, mgr) -- values()안의 column은 emp05가 가지고 있는 컬럼수와 동일해야한다
+select
+empno, ename, job, mgr, hiredate, sal, comm, deptno
+from emp;
+
+drop table emp04;
+
+create table emp04 as select * from emp where 1=0;
+
+alter table emp05 add(comm number(4,0));
+
+truncate table emp05;
+
+drop table emp04 purge;
+
+create table emp01
+as 
+select * from emp where 1=0;
+
+drop table emp01 purge;
+drop table emp02 purge;
+
+create table emp01
+as 
+select * from emp where 1=0;
+
+create table emp02
+as 
+select * from emp where 1=0;
+
+insert all
+into emp01 (empno, ename, job, mgr)
+into emp02 (empno, ename, job, mgr)
+values (empno, ename, job, mgr)
+select empno, ename, job, mgr from emp;
+
+rollback;
+
+alter table emp01 drop (mgr, hiredate, sal, comm, deptno);
+alter table emp02 drop (hiredate, sal, comm, deptno); 
+
+truncate table emp02;
+
+insert all
+into emp01 values (empno, ename, job)
+into emp02 values (empno, ename, job, mgr)
+select empno, ename, job, mgr from emp;
+
+drop table dept03 purge;
+
+create table dept01 as select * from dept;
+
+update dept01 
+set loc = (select loc from dept01 where deptno=40)
+where deptno=10;
+
+select * from dept01;
+
+update dept01 
+set (dname, loc) 
+= (select dname, loc from dept01 where deptno = 30) -- set()과 select 의 column명이 일치하지않아도 된다
+where deptno = 40;
+
+rollback;
+
+
+
+create table emp01
+as
+select * from emp;
+
+update emp01
+set (empno, ename) = (select 1111, '첫째' from dual);
+
+rollback;
+
+select * from emp01;
+
+alter table emp01 add (DNAME VARCHAR2(14));
+
+update emp01 set dname = (select dname from dept where dept.deptno = emp01.deptno);
+
+--
+DELETE FROM emp01 where deptno = (select deptno from dept where dname ='SALES');
+
+alter table emp01 drop (dname);
+
+--부서번호를 기준으로 같은 값을 가진 사원 테이블과 부서 테이블 결합
+select ename, dname
+from emp, dept
+where emp.deptno = dept.deptno;
+
+select * from emp order by deptno;
+
+select ename, dname, emp.deptno 
+from emp, dept 
+where emp.deptno = dept.deptno;
+
+select e.ename, d.dname, e.deptno as edno, d.deptno as ddno
+from emp e, dept d
+where e.deptno = d.deptno
+order by ddno;
+
+select d.dname as ddnm, e.deptno as edno, d.deptno as ddno
+from emp e, dept d
+where e.deptno = d.deptno
+--group by ddnm, edno, ddno
+group by d.dname, e.deptno, d.deptno
+having(d.deptno = 10)
+order by ddno;
+
+select e.ename, d.dname
+from emp e, dept d
+where e.deptno = d.deptno
+and e.ename ='SMITH';
+
+select e.ename, d.dname, d.loc
+from emp e, dept d
+where e.deptno = d.deptno and comm is not null;
+
+select e.ename, d.dname
+from emp e, dept d
+where e.deptno = d.deptno and e.ename like '%A%';
+
+--self join: 동일 테이블 사이의 join
+select 
+e.ename "사원이름", e.empno "사원번호", d.ename "관리자이름", d.empno "관리자번호"
+from emp e, emp d
+where e.mgr = d.empno;
+
+--특정 사원을 담당하는 매니저 사원의 이름을 출력
+--work(사원 테이블)과 manager(매니저table)로 별칭 부여
+select work.ename || '의 매니저는 ' || MANAGER.ename
+from emp work, emp MANAGER
+where work.mgr = MANAGER.empno;
+
+select ename, mgr, (select distinct ename from emp e1 where e.mgr = e1.empno) mname from emp e;
